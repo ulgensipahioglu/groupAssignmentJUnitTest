94b373f46ef5911a9cc6e5964530a8bdea9bec1b
Ulgen Eraydin <ulgensipahioglu@hotmail.com>
2025-10-07 11:23:08 +0200

    test

--- unittesting/src/test/java/dev/mygroup/BookTest.java
+++ unittesting/src/test/java/dev/mygroup/BookTest.java
@@ -3,53 +3,6 @@ package dev.mygroup;
 import org.junit.Test;
 
 public class BookTest {
-    @Test
-    public void testAdvanceDay() {
-
+    
     }
 
-    @Test
-    public void testBorrowBook() {
-
-    }
-
-    @Test
-    public void testCheckLateFee() {
-
-    }
-
-    @Test
-    public void testExtendTime() {
-
-    }
-
-    @Test
-    public void testGetAuthor() {
-
-    }
-
-    @Test
-    public void testGetDaysBorrowed() {
-
-    }
-
-    @Test
-    public void testGetGenre() {
-
-    }
-
-    @Test
-    public void testGetName() {
-
-    }
-
-    @Test
-    public void testIsBorrowed() {
-
-    }
-
-    @Test
-    public void testReturnBook() {
-
-    }
-}
