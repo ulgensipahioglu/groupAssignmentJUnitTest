4fa2f9b6f5a69021ac3f50b5bed29c6ee6d7f0f5
Nimara Islam <nimaranimara@gmail.com>
2025-10-15 10:04:40 +0200

    updated

--- src/test/java/dev/mygroup/LibraryTest.java
+++ src/test/java/dev/mygroup/LibraryTest.java
@@ -6,45 +6,16 @@ import org.junit.Test;
 
 public class LibraryTest {
 
+    //1.1:The user caanot borrow a book which is not listed in the stock list
     @Test
-    public void testAdvanceDay() {
-
+    public void testUserShouldBorrowBookFromStockList() {
         Library library = new Library();
-        library.borrowBook("Harry Potter");
-        library.advanceDay();
-        int currentDay = library.listBorrowedBooks(false).get(0).getDaysBorrowed();
-        assertTrue(currentDay == 1);
+        library.borrowBook("Abcde");
+        int borrowedBook = library.listBorrowedBooks(false).size();
+        assertTrue(borrowedBook == 0);
     }
 
-    @Test
-    public void testUserCanBorrowOneBookPerDay() {
-        Library library = new Library();
-        library.borrowBook("Harry Potter");
-        int totalBorrowedBook = library.listBorrowedBooks(false).size();
-        for (int i = 0; i < totalBorrowedBook; i++) {
-            Book book = library.listBorrowedBooks(false).get(i);
-            if (book.getDaysBorrowed() == 0) {
-                library.borrowBook("Hitchhiker's guide to the galaxy");
-            }
-        }
-        assertTrue(totalBorrowedBook == 1);
-    }
-
-    @Test
-    public void testBorrowBookAddsToBorrowedList() {
-        Library library = new Library();
-        library.borrowBook("Harry Potter");
-        int totalBorrowedBook = library.listBorrowedBooks(false).size();
-        boolean found = false;
-        for (int i = 0; i < totalBorrowedBook; i++) {
-            if (library.listBorrowedBooks(false).get(i).getName().equalsIgnoreCase("Harry Potter")) {
-                found = true;
-                break;
-            }
-        }
-        assertTrue(found);
-    }
-
+    // 1.2:The book is marked as borrowed or not
     @Test
     public void testMarkBookAsBorrowed() {
         Library library = new Library();
@@ -60,41 +31,62 @@ public class LibraryTest {
         }
     }
 
+    //1.3:The booked is listed in list of borrowed book
+    @Test
+    public void testBorrowBookAddsToBorrowedList() {
+        Library library = new Library();
+        library.borrowBook("Harry Potter");
+        int totalBorrowedBook = library.listBorrowedBooks(false).size();
+        boolean found = false;
+        for (int i = 0; i < totalBorrowedBook; i++) {
+            if (library.listBorrowedBooks(false).get(i).getName().equalsIgnoreCase("Harry Potter")) {
+                found = true;
+                break;
+            }
+        }
+        assertTrue(found);
+    }
+
+    //1.4:Checking borrow list and stock list before and after borrowing a book
     @Test
     public void testBooksInStockListSouldDecreaseDuringBorrowing() {
         Library library = new Library();
-        int availableBookListBeforeBorrow = library.listAvailableBooks().size();
-        int availableBookListAfterBorrow = 0;
+        int availableStockListBefore = library.listAvailableBooks().size();
+        int borrowedBookListBefore = library.listBorrowedBooks(false).size();
+        library.borrowBook("Harry Potter");
+        int availableStockListAfter = library.listAvailableBooks().size();
+        int borrowedBookListAfter = library.listBorrowedBooks(false).size();
+        assertTrue((availableStockListAfter == availableStockListBefore - 1)
+                && (borrowedBookListAfter == borrowedBookListBefore + 1));
+
+    }
+
+    //1.4:Updating the current day
+    @Test
+    public void testAdvanceDay() {
+        Library library = new Library();
+        library.borrowBook("Harry Potter");
+        library.advanceDay();
+        int currentDay = library.listBorrowedBooks(false).get(0).getDaysBorrowed();
+        assertTrue(currentDay == 1);
+    }
+
+    //1.5:The user may only borrow 1 book per day.
+    @Test
+    public void testUserCanBorrowOneBookPerDay() {
+        Library library = new Library();
         library.borrowBook("Harry Potter");
         int totalBorrowedBook = library.listBorrowedBooks(false).size();
         for (int i = 0; i < totalBorrowedBook; i++) {
             Book book = library.listBorrowedBooks(false).get(i);
-            if (book.getName().equalsIgnoreCase("Harry Potter") && book.isBorrowed()) {
-                availableBookListAfterBorrow = library.listAvailableBooks().size();
+            if (book.getDaysBorrowed() == 0) {
+                library.borrowBook("Hitchhiker's guide to the galaxy");
             }
-            assertTrue(availableBookListAfterBorrow == availableBookListBeforeBorrow - 1);
         }
+        assertTrue(totalBorrowedBook == 1);
     }
 
-    @Test
-    public void testUserCannotBorrowSameBookTitleAgain() {
-        Library library = new Library();
-        library.borrowBook("Ondskan");
-        int borrowedBefore = library.listBorrowedBooks(false).size();
-        library.advanceDay();
-        library.borrowBook("Ondskan");
-        int borrowedAfter = library.listBorrowedBooks(false).size();
-        assertEquals(borrowedAfter, borrowedBefore);
-    }
-
-    @Test
-    public void testUserShouldBorrowBookFromStockList() {
-        Library library = new Library();
-        library.borrowBook("Abcde");
-        int borrowedBook = library.listBorrowedBooks(false).size();
-        assertTrue(borrowedBook == 0);
-    }
-
+    //1.5:The user may at most have 5 borrowed books at a time.
     @Test
     public void testUserCannotBorrowBookMoreThan5Times() {
         Library library = new Library();
@@ -111,9 +103,22 @@ public class LibraryTest {
         library.advanceDay();
         library.borrowBook("The Great Gatsby");
         int borrowdBooklistSize = library.listBorrowedBooks(false).size();
-        assertTrue(borrowdBooklistSize <= 5);
+        assertEquals(5, borrowdBooklistSize);
     }
 
+    //1.5:The user may only borrow one book per title. .
+    @Test
+    public void testUserCannotBorrowSameBookTitleAgain() {
+        Library library = new Library();
+        library.borrowBook("Ondskan");
+        int borrowedBefore = library.listBorrowedBooks(false).size();
+        library.advanceDay();
+        library.borrowBook("Ondskan");
+        int borrowedAfter = library.listBorrowedBooks(false).size();
+        assertEquals(borrowedAfter, borrowedBefore);
+    }
+
+    // 2. The user may return as many books as he/she wishes per day.
     @Test
     public void testUserCanRetrunBookAsManyBooksPerDay() {
         Library library = new Library();
